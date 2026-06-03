9ac4db3903a5d741992a9794bdbef0fd7427e123
aaronoduffy <65084886+aaronoduffy@users.noreply.github.com>
2026-06-03 13:41:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const { pollAll } = require("./poller");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // ── Horses ───────────────────────────────────────────────────────────────────
 
